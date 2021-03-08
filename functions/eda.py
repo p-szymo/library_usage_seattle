48e868f37a7ed75c416df3d96e6824abce6d9208
“p-szymo” <joshua.szymanowski@gmail.com>
2021-03-08 10:14:39 -0500

    build save process into plotting functions

--- functions/eda.py
+++ functions/eda.py
@@ -10,7 +10,7 @@ import seaborn as sns; sns.set_style('ticks')
 from statsmodels.tsa.seasonal import seasonal_decompose
 
 
-def ts_decompose(target):
+def ts_decompose(target, save=False, filepath='ts_decompose.png'):
 
     '''
     Function to produce a prettified plot of the seasonal decomposition of data.
@@ -19,6 +19,16 @@ def ts_decompose(target):
     -----
     target : Pandas Series
         Input data.
+
+
+    Optional input
+    --------------
+    save : bool
+        Whether or not to save plot (default=False).
+
+    filepath : str
+        Desired file path where to save and file name
+            (default = 'ts_decompose.png').
 
 
     Output
@@ -88,12 +98,23 @@ def ts_decompose(target):
 
     # show plot
     plt.tight_layout()
+
+    # save (optional)
+    if save:
+        plt.savefig(filepath, bbox_inches='tight', transparent=True)
+
     plt.show()
 
     return fig
 
 
-def ts_rolling(target, period='W'):
+def ts_rolling(
+    target,
+    period='W',
+    title='Rolling Mean & Standard Deviation',
+    save=False,
+    filepath='ts_rolling.png'
+    ):
     
     '''
     Function to produce a prettified plot with time series data, rolling mean, and
@@ -113,6 +134,16 @@ def ts_rolling(target, period='W'):
             Monthly = 'M',
             Bi-annually (6 months) = 'B',
             Yearly = 'Y'
+
+    title : str
+        Title for graph (default = 'Rolling Mean & Standard Deviation')
+
+    save : bool
+        Whether or not to save plot (default=False).
+
+    filepath : str
+        Desired file path where to save and file name
+            (default = 'ts_rolling.png').
 
 
     Output
@@ -136,9 +167,21 @@ def ts_rolling(target, period='W'):
             'B': 180,
             'Y': 365
         }
+
+        # convert to integer
+        period = period_dict[period]
+
         # determine rolling statistics
-        roll_mean = target.rolling(window=period_dict[period], center=False).mean()
-        roll_std = target.rolling(window=period_dict[period], center=False).std()
+        roll_mean = target.rolling(window=period, center=False).mean()
+        roll_std = target.rolling(window=period, center=False).std()
+
+    # capture date objects
+    start_month = target.index[0].strftime('%B')
+    start_day = target.index[0].strftime('%d')
+    start_year = target.index[0].strftime('%Y')
+    end_month = target.index[-1].strftime('%B')
+    end_day = target.index[-1].strftime('%d')
+    end_year = target.index[-1].strftime('%Y')
         
     # plot rolling statistics
     fig = plt.figure(figsize=(20,10))
@@ -148,10 +191,19 @@ def ts_rolling(target, period='W'):
 
     # prettify
     plt.legend(loc='best', fontsize=20)
-    plt.title('Rolling Mean & Standard Deviation', fontsize=28, pad=20)
+    plt.title(
+        title + f' ({period} days)\n' + f'{start_month} {start_day}, {start_year} - {end_month} {end_day}, {end_year}',
+        fontsize=28, pad=20)
     plt.ylabel('Number of checkouts', fontsize=25, labelpad=15)
     plt.xticks(fontsize=22)
     plt.yticks(fontsize=20)
+
+    # show plot
+    plt.tight_layout()
+
+    # save (optional)
+    if save:
+        plt.savefig(filepath, bbox_inches='tight', transparent=True)
 
     plt.show()
 
